babb06ddb2ae008a50603b81e1a6b82893ae21cc
YuanArib <yuan.arib@gmail.com>
2024-12-31 18:45:17 +0700

    INITIAL COMMIT

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    theme: {
+      extend: {
+        backgroundImage: {
+          'custom-gradient': 'linear-gradient(45deg, #252945, rgba(0, 0, 0, 0))',
+        },
+      },
+    },
+    plugins: [],
+  };
+  
